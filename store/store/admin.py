7d5f7d2f6bf8a177fd5766218fe02d89881c9922
software1398 <52308055+software1398@users.noreply.github.com>
2019-07-18 21:18:58 +0900

    ver0.1.6

--- store/store/admin.py
+++ store/store/admin.py
@@ -1,24 +1,54 @@
 from django.contrib import admin
-from store.models import Storedb
+from .models import Storedb, Accountdb, Queuedb
 
 
 # Register your models here.
 
 class StoreAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,      {'fields':['storenum']}),
-        ('Name',    {'fields':['storename']}),
-        ('Catagory',    {'fields':['category']}),
-        ('address',     {'fields':['address']}),
-        ('phone',       {'fields':['phonenum']}),
-        ('openclose',   {'fields':['openclose']}),
-        ('latencytime', {'fields':['latencytime']}),
+        (None,          {'fields': ['storenum']}),
+        ('Name',        {'fields': ['storename']}),
+        ('Catagory',    {'fields': ['category']}),
+        ('Latitude',    {'fields': ['latitude']}),
+        ('Longitude',   {'fields': ['longitude']}),
+        ('Intro',       {'fields': ['intro']}),
+        ('Menu',        {'fields': ['menu']}),
+        ('Inform',      {'fields': ['inform']}),
+        ('latencytime', {'fields': ['latencytime']})
     ]
 
-    list_display = ['storenum','storename','category','address','phonenum','openclose','latencytime']
+    list_display = ['storenum', 'storename', 'category', 'intro', 'menu', 'inform', 'latencytime']
     list_filter = ['category']
-    search_fields = ['storenum','storename','phonenum']
+    search_fields = ['storenum','storename']
     ordering = ('storenum',)
 
 
+class StoreManager(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields': ['storenum']}),
+        ('ID',          {'fields': ['storeid']}),
+        ('PWD',         {'fields': ['storepwd']})
+    ]
+
+    list_display = ['storenum', 'storeid', 'storepwd']
+    ordering = ('storenum',)
+
+
+class QueueAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields': ['barcode']}),
+        ('OnOff',       {'fields': ['onoffline']}),
+        ('storenum',    {'fields': ['storenum']}),
+        ('CreateTime',  {'fields': ['createtime']}),
+        ('UpdateTime',  {'fields': ['updatetime']}),
+        ('status',      {'fields': ['status']})
+    ]
+
+    list_display = ['barcode', 'onoffline', 'storenum', 'createtime', 'updatetime', 'status']
+    list_filter = ['storenum']
+    ordering = ('createtime',)
+
+
 admin.site.register(Storedb, StoreAdmin)
+admin.site.register(Accountdb, StoreManager)
+admin.site.register(Queuedb, QueueAdmin)
--- store/store/models.py
+++ store/store/models.py
@@ -8,21 +8,40 @@
 from django.db import models
 
 
+class Accountdb(models.Model):
+    storenum = models.ForeignKey('Storedb', models.DO_NOTHING, db_column='storenum')
+    storeid = models.CharField(primary_key=True, max_length=20)
+    storepwd = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'accountdb'
+
+
+class Queuedb(models.Model):
+    barcode = models.CharField(primary_key=True, max_length=20)
+    onoffline = models.BooleanField()
+    storenum = models.ForeignKey('Storedb', models.DO_NOTHING, db_column='storenum')
+    createtime = models.DateTimeField()
+    updatetime = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=10)
+
+    class Meta:
+        managed = False
+        db_table = 'queuedb'
+
+
 class Storedb(models.Model):
     storenum = models.CharField(primary_key=True, max_length=10)
     storename = models.CharField(max_length=50)
     category = models.CharField(max_length=20)
-    coordinates = models.CharField(max_length=40)
+    latitude = models.CharField(max_length=40)
+    longitude = models.CharField(max_length=40)
     intro = models.CharField(max_length=200, blank=True, null=True)
-    address = models.CharField(max_length=120, blank=True, null=True)
-    phonenum = models.CharField(max_length=15, blank=True, null=True)
     menu = models.CharField(max_length=300, blank=True, null=True)
-    inform = models.CharField(max_length=300, blank=True, null=True)
-    openclose = models.CharField(max_length=10)
+    inform = models.CharField(max_length=500, blank=True, null=True)
     latencytime = models.CharField(max_length=10)
 
     class Meta:
         managed = False
         db_table = 'storedb'
-
-
