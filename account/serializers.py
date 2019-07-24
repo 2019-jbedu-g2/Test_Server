3f2fb595a24c994a08bba5982f061822df629288
software1398 <scorpio1398@hanmail.net>
2019-07-24 11:41:35 +0900

    a

--- account/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Queuedb
-from rest_framework import serializers
-
-
-class QueueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Queuedb
-        fields = ('barcode', 'onoffline', 'status')
